4d74012fdcbbc52ee8806132eedb6403e75713cf
Vinh Vu <43430238+vinhvu97@users.noreply.github.com>
2019-08-15 21:00:18 -0700

    Add files via upload

---
+++ Lab 7/Lab7_TopModule.v
@@ -0,0 +1,78 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// File Name: Lab7_TopModule
+// Project: Lab 7
+// Designer: Zachery takkesh & Vinh Vu
+// Email: zachtakkesh@gmail.com & vincentvinhvu@yahoo.com
+// Rev. Date: April 16, 2018
+//
+// Purpose: 
+//
+// Notes:   
+////////////////////////////////////////////////////////////////////////////////
+module Lab7_TopModule(clk, reset, step, mem_W_en, adr_sel, s_sel, pc_ld, pc_inc, 
+							 reg_W_en, ir_ld, a, b, c, d, e, f, g, anode, C, N, Z);
+	// Input declarations						 
+	input clk   , reset , step   , mem_W_en , adr_sel, 
+	      s_sel , pc_ld , pc_inc , reg_W_en , ir_ld ;
+	
+	// Output declarations
+	output       a , b , c , d , e , f , g;
+	output       C , N , Z;
+	output [7:0] anode;
+	
+	// Wire declarations to interconnect the modules 
+	wire        wire_clock , wire_shot , ram_shot;
+	wire [15:0] Din        , Dout;
+	wire [15:0] addr;
+	
+	// Instantiate the clock divider module to generate a 500Hz clock
+	clk_500Hz          clk0        (.clk_in(clk), 
+									        .reset(reset), 
+									        .clk_out(wire_clock));
+	
+   // Instantiate two one-shot debounces to create two one-shot's.  
+	// One is for the control processing unit and one is for the RAM. 
+	debounce           shot_0      (.clk(wire_clock), 
+										     .reset(reset), 
+										     .D_in(step), 
+										     .D_out(wire_shot));
+	debounce			    shot_RAM    (.clk(wire_clock), 
+										     .reset(reset), 
+										     .D_in(mem_W_en), 
+										     .D_out(ram_shot)); 
+	
+	// Instantiate the CPU_EU to execute functions based on user inputs 
+	// which come from the board's switches 
+	CPU_EU             cpu0        (.clk(wire_shot), 
+									        .reset(reset), 
+									        .din(Din), 
+									        .address(addr), 
+									        .dout(Dout), 
+									        .adr_sel(adr_sel),
+					                    .s_sel(s_sel), 
+									        .pc_ld(pc_ld), 
+									        .pc_inc(pc_inc), 
+									        .reg_W_en(reg_W_en), 
+									        .ir_ld(ir_ld), 
+					                    .C(C), .N(N), .Z(Z));
+	
+	// Instantiate the RAM memory module to store or to write memory from
+	// the CPU_EU module 
+	ram                ram0        (.clk(clk), 
+	                                .we(ram_shot), 
+									        .addr({addr[7:0]}), 
+									        .din(Dout), 
+									        .dout(Din));
+	
+	// Instantiate the display controller to output Dout and address
+	// to the 7-display segment 
+	display_controller controller0 (.clk(clk), 
+	                                .reset(reset),
+											  .S(Dout), 
+											  .R(addr), 
+											  .anode(anode), 
+							              .a(a), .b(b), .c(c), .d(d), 
+											  .e(e), .f(f), .g(g));
+	
+endmodule
---
+++ Lab 7/PC.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// File Name: PC
+// Project: Lab 7
+// Designer: Zachery takkesh & Vinh Vu
+// Email: zachtakkesh@gmail.com & vincentvinhvu@yahoo.com
+// Rev. Date: April 16, 2018
+//
+// Purpose: Program counter module inputs Alu_Out from the IDP module. There are
+//				two signals for two different functions that can be done with the
+//          input data. When load is 0 and increment 1, Q gets incremented. 
+//				When load is 1 and increment is 0, D goes to Q. 
+//
+// Notes:   The program counter cannot increments and output the results at the
+//			   same time. The module can only perform 2 functions, everything else is 
+//			   just load Q to Q.  
+////////////////////////////////////////////////////////////////////////////////
+module PC(clk, reset, ld, inc, D, Q);
+
+	// Input declarations
+	input        clk , reset , ld , inc;
+	input [15:0] D;
+	
+	// Output declarations
+	output reg [15:0] Q;
+	
+	always@(posedge clk or posedge reset)
+	
+		// If reset is asserted, pc_out is always 0
+		if(reset == 1'b1)
+			Q <= 16'b0;
+		else 
+			
+			// Case for load and increment, active high
+			case({ld, inc})
+				2'b0_1: Q <= Q + 16'b1; // increment Q
+				2'b1_0: Q <= D;    // load D
+				default: Q <= Q;	  // Q gets Q as default
+			endcase 
+			
+endmodule
